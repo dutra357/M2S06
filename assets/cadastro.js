a8628a646a92513c35c3213ca2d4c4e057306cf6
David Alves Dutra <dadutra@hotmail.com>
2024-06-24 18:27:50 -0300

    Ex. 03 - LabScore (Pt.3).

--- assets/cadastro.js
+++ assets/cadastro.js
@@ -7,12 +7,26 @@ function handleCEP(){
     const viaCep = fetch(`https://viacep.com.br/ws/${inputCep}/json/`);
     viaCep.then(r => r.json())
     .then(body => {
-        console.log("Processando..");
+        retorno.innerHTML = "";
+        retorno.className = "";
+
         document.querySelector('#estado').setAttribute('placeholder', `${body.uf}`);
         document.querySelector('#cidade').setAttribute('placeholder', `${body.localidade}`);
         document.querySelector('#rua').setAttribute('placeholder', `${body.logradouro}`);
-        retorno.innerHTML = "";
-        retorno.className = "";
+
+        let nome = document.querySelector('#nome').value;
+        let idade = document.querySelector('#idade').value;
+        let serie = document.querySelector('#serie').value;
+        let escola = document.querySelector('#escola').value;
+        let materia = document.querySelector('#materia').value;
+
+        localStorage.setItem("nome", nome);
+        localStorage.setItem("idade", idade);
+        localStorage.setItem("serie", serie);
+        localStorage.setItem("escola", escola);
+        localStorage.setItem("materia", materia);
+
+        window.location.href = 'notas.html';
 
     })
     .catch(() => {
